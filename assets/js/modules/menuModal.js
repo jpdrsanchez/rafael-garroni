5f13a2be9e7f3897a656e2ac5511f780c07174f2
João Pedro <joaopedrosanchez302@gmail.com>
2021-01-27 13:39:38 -0300

    menu modal mobile

--- assets/js/modules/menuModal.js
+++ assets/js/modules/menuModal.js
@@ -4,8 +4,14 @@ const initMenuModal = () => {
   const menu = document.querySelector('[data-menu="body"]');
 
   if (open && close && menu) {
-    open.addEventListener('click', () => menu.classList.add('active'));
-    close.addEventListener('click', () => menu.classList.remove('active'));
+    open.addEventListener('click', () => {
+      menu.classList.add('active');
+      document.body.style.overflow = 'hidden';
+    });
+    close.addEventListener('click', () => {
+      menu.classList.remove('active');
+      document.body.style.overflow = '';
+    });
   }
 };
 
