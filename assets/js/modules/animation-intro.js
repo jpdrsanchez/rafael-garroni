e0d34b52eaf9a9fa2970885429b5c6c136f7e230
João Pedro <joaopedrosanchez302@gmail.com>
2021-01-28 12:29:33 -0300

    ajustes finais

--- assets/js/modules/animation-intro.js
+++ assets/js/modules/animation-intro.js
@@ -1,10 +1,17 @@
 const initIntroAnimation = () => {
   const introElement = document.querySelector('[data-animation="intro"]');
 
-  if (introElement)
-    introElement.addEventListener('animationend', () => {
-      introElement.remove();
-    });
+  if (sessionStorage.getItem('animation') !== 'yes') {
+    sessionStorage.setItem('animation', 'no');
+
+    if (introElement)
+      introElement.addEventListener('animationend', () => {
+        introElement.remove();
+        sessionStorage.setItem('animation', 'yes');
+      });
+  } else {
+    if (introElement) introElement.remove();
+  }
 };
 
 export default initIntroAnimation;
